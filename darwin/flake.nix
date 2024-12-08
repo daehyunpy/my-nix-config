81ac01dac9685730b9285a95e6e963aeb2ddd045
유대현 <dh.you@60hz.io>
2024-12-08 09:04:42 +0900

    Initial commit

---
+++ darwin/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Daehyun's nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nix-darwin.url = "github:LnL7/nix-darwin";
+    nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nix-darwin, nixpkgs }:
+  let
+    configuration = { pkgs, ... }: {
+      environment.systemPackages = import ../packages.nix pkgs;
+
+      nix.settings.experimental-features = "nix-command flakes";
+
+      programs.fish.enable = true;
+
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      system.stateVersion = 5;
+
+      environment.shells = [ pkgs.fish ];
+
+      nixpkgs.config.allowUnfree = true;
+      nixpkgs.hostPlatform = "aarch64-darwin";
+    };
+  in
+  {
+    darwinConfigurations.thales = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+  };
+}
---
+++ packages.nix
@@ -0,0 +1,6 @@
+pkgs: [
+  pkgs.fish
+  pkgs.neovim
+  pkgs.tmux
+  pkgs.vscode
+]
