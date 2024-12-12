9ce7df84b23298953323ff22d2faa5453f27b013
Daehyun You <dh.you@60hz.io>
2024-12-12 19:48:35 +0900

    Add Homebrew Casks

--- darwin/flake.nix
+++ darwin/flake.nix
@@ -5,30 +5,48 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
 
-  outputs = { self, nix-darwin, nixpkgs }:
+  outputs = { self, nix-darwin, nixpkgs, nix-homebrew }:
   let
     configuration = { pkgs, ... }: {
-      environment.systemPackages = import ../packages.nix pkgs;
-
       nix.settings.experimental-features = "nix-command flakes";
+
+      environment.systemPackages =
+        builtins.map
+        (name: pkgs.${name})
+        (import ../packages.nix);
+      environment.shells = [ pkgs.fish ];
+
+      homebrew = {
+        enable = true;
+        casks = import ./casks.nix;
+      };
 
       programs.fish.enable = true;
 
       system.configurationRevision = self.rev or self.dirtyRev or null;
-
       system.stateVersion = 5;
-
-      environment.shells = [ pkgs.fish ];
 
       nixpkgs.config.allowUnfree = true;
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users.daehyun.shell = "${pkgs.fish}/bin/fish";
     };
   in
   {
     darwinConfigurations.morpheus = nix-darwin.lib.darwinSystem {
-      modules = [ configuration ];
+      modules = [
+        configuration
+        nix-homebrew.darwinModules.nix-homebrew {
+          nix-homebrew = {
+            enable = true;
+            enableRosetta = true;
+            user = "daehyun";
+          };
+        }
+      ];
     };
 
     darwinConfigurations.thales = nix-darwin.lib.darwinSystem {
