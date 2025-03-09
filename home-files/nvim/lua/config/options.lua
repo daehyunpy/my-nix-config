a080b0c562d9f837ff244c27e3bce9a4d31f1013
Daehyun You <dh.you@60hz.io>
2025-03-09 20:43:44 +0900

    feat(nvim/tmux): enhance clipboard integration with tmux for better copy-paste functionality The configuration now checks if the application is running inside tmux and sets up the clipboard options accordingly. This allows seamless integration between Neovim and tmux for copying and pasting, improving the overall user experience. Additionally, tmux settings are updated to allow passthrough and enable clipboard support, further enhancing usability.

--- home-files/nvim/lua/config/options.lua
+++ home-files/nvim/lua/config/options.lua
@@ -3,5 +3,20 @@
 -- Add any additional options here
 vim.g.python3_host_prog = "python"
 
+if os.getenv("TMUX") or "" ~= "" then
+  vim.g.clipboard = {
+    name = "tmux",
+    copy = {
+      ["+"] = "tmux load-buffer -w -",
+      ["*"] = "tmux load-buffer -w -",
+    },
+    paste = {
+      ["+"] = "tmux save-buffer -",
+      ["*"] = "tmux save-buffer -",
+    },
+    cache_enabled = 1,
+  }
+end
+
 vim.opt.clipboard = "unnamedplus"
 vim.opt.colorcolumn = "72,79"
