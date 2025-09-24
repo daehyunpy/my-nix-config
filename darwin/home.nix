b8da82f9f946d235c592a0928262d1590ec14578
Daehyun You <daehyun.park.you@proton.me>
2025-09-25 01:22:26 +0900

    Add Cursor configuration symlink to Darwin home.nix

--- darwin/home.nix
+++ darwin/home.nix
@@ -1,6 +1,15 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+  homePathString = config.home.homeDirectory;
+  makeOutOfStore = config.lib.file.mkOutOfStoreSymlink;
+in
 {
   home.packages = import ./packages.nix pkgs;
+  home.file = {
+    "Library/Application Support/Cursor/User".source =
+      makeOutOfStore "${homePathString}/.config/nix/home-files/cursor";
+  };
+
   programs.fish = {
     interactiveShellInit = ''
       eval /opt/homebrew/Caskroom/miniconda/base/bin/conda shell.fish hook | source
