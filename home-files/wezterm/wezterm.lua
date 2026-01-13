88efa3610941dae0bee5c06edb04d93acfb053aa
Daehyun You <daehyun.park.you@proton.me>
2026-01-13 11:08:05 -0800

    Change WezTerm color scheme to GitHub

--- home-files/wezterm/wezterm.lua
+++ home-files/wezterm/wezterm.lua
@@ -2,6 +2,6 @@ local wezterm = require("wezterm")
 
 return {
 	font = wezterm.font("ZedMono Nerd Font"),
-	color_scheme = "Tokyo Night Day",
+	color_scheme = "Github",
 	quit_when_all_windows_are_closed = false,
 }
