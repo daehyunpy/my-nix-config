f19646110a03d36b98165678b36568e8d4a0f46f
Daehyun You <daehyun.park.you@proton.me>
2026-01-15 23:51:14 -0800

    Add dynamic color scheme based on system appearance

--- home-files/wezterm/wezterm.lua
+++ home-files/wezterm/wezterm.lua
@@ -1,7 +1,22 @@
 local wezterm = require("wezterm")
 
+local function get_appearance()
+    if wezterm.gui then
+        return wezterm.gui.get_appearance()
+    end
+    return "Dark"
+end
+
+local function scheme_for_appearance(appearance)
+    if appearance:find "Dark" then
+        return "GitHub Dark"
+    else
+        return "Github"
+    end
+end
+
 return {
-	font = wezterm.font("ZedMono Nerd Font"),
-	color_scheme = "Github",
-	quit_when_all_windows_are_closed = false,
+    font = wezterm.font("ZedMono Nerd Font"),
+    color_scheme = scheme_for_appearance(get_appearance()),
+    quit_when_all_windows_are_closed = false,
 }
