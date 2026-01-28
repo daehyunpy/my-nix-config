9cb720645fd38465d9a3fbc04d6f7ad0706da419
Daehyun You <daehyun.park.you@proton.me>
2026-01-28 15:22:55 -0800

    Update Apple application IDs in mas.nix

--- darwin/mas.nix
+++ darwin/mas.nix
@@ -5,13 +5,13 @@
   "grammarly" = 1462114288;
   "jump-desktop" = 524141863;
   "keka" = 470158793;
-  "keynote" = 409183694;
+  "keynote" = 361285480;
   "microsoft-excel" = 462058435;
   "microsoft-powerpoint" = 462062816;
   "microsoft-word" = 462054704;
   "mountain-duck" = 1024974133;
-  "numbers" = 409203825;
-  "pages" = 409201541;
+  "numbers" = 361304891;
+  "pages" = 361309726;
   "pdf-expert" = 1055273043;
   "slack" = 803453959;
   "tailscale" = 1475387142;
